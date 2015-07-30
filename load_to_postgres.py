c3c4d5f6e24eb436e44c3eb9bb01b294d334a8f8
rquinlan <rquinlan@cogolabs.com>
2015-07-30 19:26:45 -0400

    loads empty tables to pg

--- load_to_postgres.py
+++ load_to_postgres.py
@@ -1,6 +1,66 @@
 import psycopg2
 from psycopg2.extras import DictCursor
 import configs
+import csv
+import os
+import re
+from pprint import pprint
 
+# POSTGRES is a dict in the global scope of the gitignored configs.py file specifying db and auth
 pg_connection = psycopg2.connect(**configs.POSTGRES)
+pg_connection.autocommit = True
 pg_cursor = pg_connection.cursor(cursor_factory=DictCursor)
+
+
+def table_from_csv(file_name, column_names):
+    try:
+        # I know formatting this is a bad habit but psycopg2 won't allow me to pass
+        # in a table name proper regular way
+        create_table_query =\
+            """ CREATE TABLE {table_name} (
+                    "DATE" date not null,
+                    {data_columns}
+                );
+            """.format(
+                table_name=file_name,
+                data_columns=reduce(
+                    lambda column_list, column: column_list + column,
+                    [
+                        '{} float,\n'.format(
+                            re.sub(
+                                '[^0-9a-zA-Z_]+',
+                                '',
+                                column_name.replace(' ', '_')
+                            )
+                        )
+                        for column_name in column_names\
+                        if column_name not in ['DATE', '']
+                    ]
+                )[0:-2]
+            )
+        pg_cursor.execute(create_table_query)
+    except Exception as e:
+        print 'Error: ', e
+        if 'already exists' not in str(e):
+            print '\n', create_table_query
+
+def load_to_postgres(csv_file_path):
+    with open(csv_file_path, 'r') as csv_file:
+        csv_contents = csv.DictReader(csv_file)
+        for index, row in enumerate(csv_contents):
+            row = dict(row)
+            if index == 0:
+                table_from_csv(
+                    csv_file_path.split('/')[-1].replace('.csv', ''),
+                    row.keys()
+                )
+
+            # pprint(row)
+
+def main():
+    for csv_file in os.listdir('csv_data'):
+        load_to_postgres('csv_data/' + csv_file)
+
+
+if __name__ == '__main__':
+    main()
