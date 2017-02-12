a119a22897f023465af0f361053207ba516b7074
Rory Quinlan <rjamesquinlan@gmail.com>
2017-02-12 11:28:34 -0300

    delete mistakenly added example code

--- dataviz/app/src/components/chart.js
+++ dataviz/app/src/components/chart.js
@@ -28,24 +28,3 @@ const Chart = ({}) => (
 )
 
 export default Chart
-
-import fetch from 'isomorphic-fetch'
-
-export const REQUEST_POSTS = 'REQUEST_POSTS'
-export const RECEIVE_POSTS = 'RECEIVE_POSTS'
-export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT'
-export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT'
-
-export function selectSubreddit(subreddit) {
-  return {
-    type: SELECT_SUBREDDIT,
-    subreddit
-  }
-}
-
-export function invalidateSubreddit(subreddit) {
-  return {
-    type: INVALIDATE_SUBREDDIT,
-    subreddit
-  }
-}
